a1438fbe58261ed9cae11b4ff72d92655a2781f9
SickCrisp <crissie.nicholas.r@gmail.com>
2025-08-03 23:32:30 -0700

    Shutting down for the night (3AUG2025)

--- main.py
+++ main.py
@@ -2,19 +2,18 @@ import pygame, constants
 
 def main():
     pygame.init()
-
-    print("Starting Asteroids!")
-    print(f"Screen width: {constants.SCREEN_WIDTH}")
-    print(f"Screen height: {constants.SCREEN_HEIGHT}")
-
     screen = pygame.display.set_mode((constants.SCREEN_WIDTH, constants.SCREEN_HEIGHT))
+    clock = pygame.time.Clock()
+    dt = 0
     
     while True:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
-        pygame.Surface.fill(0, 0, 0)
+        screen.fill("black")
         pygame.display.flip()
+
+        dt = clock.tick(60) / 1000
 
 
 if __name__ == "__main__":
